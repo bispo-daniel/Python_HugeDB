262b73b4f18fc5d4c6362a766cd9ef33ce91e734
bispo-daniel <dbispo.work@gmail.com>
2023-03-31 13:39:41 -0300

    Update hugeDB.sql

--- hugeDB.sql
+++ hugeDB.sql
@@ -5,15 +5,16 @@ use hugeDB;
 create table person (
 	id int(9) not null auto_increment,
     nome varchar(30) not null,
-    dia1 DATETIME ON UPDATE CURRENT_TIMESTAMP,
+    dia1 DATETIME DEFAULT NOW(),
 	dia2 DATETIME DEFAULT NOW(),
     primary key(id)
 );
 
-/*ALTER TABLE person DROP COLUMN dia1;*/
+/*ALTER TABLE person ADD dia3 DATETIME DEFAULT NOW();*/
+/*ALTER TABLE person ADD dia4 DATETIME DEFAULT NOW();*/
 /*ALTER TABLE person ADD dia5 DATETIME DEFAULT NOW();*/
 
 /*insert into person (nome) values 
-("dan"), ("den"), ("din"), ("don"), ("dun")*/
+("dan"), ("den"), ("din"), ("don"), ("dun");*/
 
-insert into person (nome) values ("finally")
+insert into person (nome) values ("filomena");
